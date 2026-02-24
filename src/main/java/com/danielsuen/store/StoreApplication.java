2a20980e1b4ecb2fa4383cbab715ec533df1b671
danielsuen523 <danielsuen523@gmail.com>
2026-02-24 22:32:58 +0800

    Refractor code with lombok

--- src/main/java/com/danielsuen/store/StoreApplication.java
+++ src/main/java/com/danielsuen/store/StoreApplication.java
@@ -1,31 +1,26 @@
 package com.danielsuen.store;
 
-import com.danielsuen.store.service.OrderService;
-import com.danielsuen.store.service.UserService;
-import org.springframework.boot.SpringApplication;
+import com.danielsuen.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        //ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-//        var orderService = context.getBean(OrderService.class);
-//        orderService.placeOrder();
+        var user = new User();
+        user.setName("Daniel");
+        user.setPassword("1234");
+        user.setEmail("test@gmail.com");
+        user.setPassword("1234");
 
-//        var notificationService = context.getBean(NotificationService.class);
-//        notificationService.send("test");
-
-        var userService = context.getBean(UserService.class);
-        User user1 = new User(1, "1@gmail.com", "abc123", "User 1");
-        User user2 = new User(2, "2@gmail.com", "abc123", "User 2");
-        User user3 = new User(3, "3@gmail.com", "abc123", "User 3");
-        userService.registerUser(user1);
-        userService.registerUser(user2);
-        userService.registerUser(user3);
-	}
-
+        // enabled by lombok @Builder
+        User.builder()
+                .name("Daniel")
+                .password("1234")
+                .email("email")
+                .build();
+    }
 }
