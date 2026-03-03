96c5a81da2cf72624da13fd4012927338f9265b0
danielsuen523 <danielsuen523@gmail.com>
2026-03-03 22:34:58 +0800

    Refractor code

--- src/main/java/com/danielsuen/store/StoreApplication.java
+++ src/main/java/com/danielsuen/store/StoreApplication.java
@@ -1,9 +1,5 @@
 package com.danielsuen.store;
 
-import com.danielsuen.store.entities.Address;
-import com.danielsuen.store.entities.Profile;
-import com.danielsuen.store.entities.Tag;
-import com.danielsuen.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,39 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-        var user = new User();
-        user.setName("Daniel");
-        user.setPassword("1234");
-        user.setEmail("test@gmail.com");
-        user.setPassword("1234");
-
-        // enabled by lombok @Builder
-        User.builder()
-                .name("Daniel")
-                .password("1234")
-                .email("email")
-                .build();
-
-        var address = Address.builder()
-                .street("123 Main St")
-                .city("Main St")
-                .state("Main St")
-                .zip("12345")
-                .build();
-
-        //
-        user.addAddress(address);
-
-        user.addTag("tag1");
-
-        var profile = Profile.builder()
-                .bio("bio")
-                .build();
-        user.setProfile(profile);
-        profile.setUser(user);
-        System.out.println(profile);
     }
 }
